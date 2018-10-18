f3c450fdf3ffbf1cf2005643a1817ca4315ad126
natebecker11 <natebecker11@gmail.com>
2018-10-18 18:15:35 -0400

    set up seeds for  departments

---
+++ departmentSeed2.sql
@@ -0,0 +1,4 @@
+USE bamazon;
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ('Books', 1000.00), ('Mobile Phones', 4000.00), ('Appliances', 10000.00), ('Video Games', 15000.00), ('Board Games', 2000.00);
