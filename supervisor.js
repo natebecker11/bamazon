f296f7e1ad8e126b645bde1c6e94da3e43fbdee7
natebecker11 <natebecker11@gmail.com>
2018-10-20 12:59:40 -0400

    finished get sales by department

--- supervisor.js
+++ supervisor.js
@@ -3,6 +3,11 @@ dotenv.config()
 const keys = require('./keys.js')
 const inquirer = require('inquirer')
 const mysql = require('mysql')
+const {table, getBorderCharacters} = require('table');
+const tableConfig = {
+  border: getBorderCharacters('honeywell')
+}
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -14,3 +19,31 @@ const connection = mysql.createConnection({
   password: keys.MySQL.pw,
   database: 'bamazon'
 })
+
+const tablify = (aoo, ...keys) => {
+  let tabled = aoo.map(obj => {
+    let row = keys.map(key => obj[key])
+    return row;
+  })
+  tabled.unshift(keys)
+  return tabled
+}
+
+
+const viewSalesByDept = (dept) => {
+  return new Promise((resolve,reject) => {
+    connection.query(
+      'SELECT departments.department_id AS id, products.department_name AS Department, departments.over_head_costs AS overhead, SUM(products.product_sales) AS TotalSales, SUM(products.product_sales) - departments.over_head_costs AS NetProfit FROM products INNER JOIN departments ON products.department_name = departments.department_name GROUP BY products.department_name ORDER BY id', (err, res) => {
+        if (err) reject(err)
+        // console.log(res)
+        resolve(res)
+      }
+    )
+  })
+  .then(results => {
+    return tablify(results, 'id', 'Department', 'overhead', 'TotalSales', 'NetProfit')
+  })
+  .then(tabled => console.log(table(tabled, tableConfig)))
+}
+
+viewSalesByDept();
